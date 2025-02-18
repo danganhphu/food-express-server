9b77ac0938645fbc3845e6cbfc2a811c90f05c77
phuda <daphu.dev@gmail.com>
2025-02-18 14:33:35 +0700

    feat (Categories): Product endpoints `Create`

---
+++ src/Services/Catalog/Services.Catalog/Features/Products/Create/v1/CreateProductHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Services.Catalog.Features.Products.Create.v1;
+
+public sealed class CreateProductHandler([FromKeyedServices("catalog")] IEfRepository<Product> repository)
+    : ICoreCommandHandler<CreateProductCommand, Result<Guid>>
+{
+    public async Task<Result<Guid>> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = Product.Create(
+            command.Name,
+            command.Size,
+            command.Price,
+            command.PriceSale,
+            command.CategoryId,
+            command.BrandId,
+            command.SupplierId);
+
+        var result = await repository.AddAsync(product, cancellationToken);
+
+        return Result.Success(result.Id.Value);
+    }
+}
---
+++ src/Services/Catalog/Services.Catalog/Features/Products/ProductMappings.cs
@@ -0,0 +1,18 @@
+﻿namespace Services.Catalog.Features.Products;
+
+internal static class ProductMappings
+{
+    internal static ProductDto ToProductDto(this Product product)
+        => new(
+            product.Id.Value,
+            product.Name.Value,
+            product.Size.Value,
+            product.Price.OriginalPrice,
+            product.Price.DiscountPrice,
+            product.Category.Name,
+            product.Brand.Name,
+            product.Supplier.Name);
+
+    internal static IReadOnlyCollection<ProductDto> ToProductDtos(this IEnumerable<Product> products)
+        => products.Select(ToProductDto).ToArray();
+}
